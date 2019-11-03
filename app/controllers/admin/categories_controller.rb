4cb794fa934a5215c4a38054a2241c41f6b5447f
EC2 Default User <ec2-user@ip-172-31-29-223.us-east-2.compute.internal>
2019-11-03 04:27:52 +0000

    nil category fixed

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,12 +25,16 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = Category.find(params[:id])
+    if(!params[:id].nil?)
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
         format.html { save_category }
-        format.js do 
+         format.js do 
           @category.save
           @article = Article.new
           @article.categories << @category
